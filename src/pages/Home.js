479aa4f829c0b542f8a88e89df058e87255da779
Kun Zhu <zkzhu@uwaterloo.ca>
2022-09-17 23:55:46 -0400

    add top bar navigation to home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,32 @@ import { Fragment } from "react";
 import { Button, View, StyleSheet, Text, Image, StatusBar } from "react-native";
 import GreetingCard from "../components/GreetingCard";
 // import MapManager from "../map-manager/map-manager";
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const TopBar = ( { navigation }) => {
+  return(
+    <Fragment style={styles.top}>
+      <FontAwesome5 
+      name={'backward'}
+      onPress={() =>
+        navigation.navigate('CameraScreen')
+      }
+       />
+      <Text>
+        Settings
+      </Text>
+    </Fragment>
+  )
+}
 
 const Home = ({ navigation }) => {
     // const mapManager = new MapManager();
   return (
     <View style={styles.container}>
+      <TopBar navigation={navigation}/>
+      <Fragment>
         <GreetingCard style={styles.greetingCard} />
+      </Fragment>
       <Button style={styles.button}
         title="Find Friend"
         onPress={() =>
@@ -19,6 +39,12 @@ const Home = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+  top: {
+    flex: 1,
+    flexOrder: "row",
+    justifyContent: "space-between",
+    fontSize: "28px",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
