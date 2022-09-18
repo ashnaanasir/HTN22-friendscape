f96664406d0ce6004a775f1151812971bebd8c32
Ayesha Nasir <43529436+ayeshanasir@users.noreply.github.com>
2022-09-17 23:56:21 -0400

    Updated styles for greeting card.

--- src/components/GreetingCard.js
+++ src/components/GreetingCard.js
@@ -4,11 +4,12 @@ import { View , Text, ProgressViewIOSComponent , StyleSheet , Image } from "reac
 const GreetingCard = () => {
     const username = 'Pika'; //TODO: Make it dynamic after adding auth
     return (
-        <View style={{width: 350, height: 300, backgroundColor: '#eee', borderRadius: 20, marginBottom: 25}}>
+        <View style={{width: 350, height: 300, backgroundColor: '#eee', borderRadius: 20, marginBottom: 50}}>
+            <View style={{width: 300, height: 20, backgroundColor: '#333', borderRadius: 10, marginTop: 20, marginLeft: 26}}></View>
             <Text style={styles.greetingHeading}>Hello, 
-            <Text style={styles.greetingUsername}> {username}</Text>
+                <Text style={styles.greetingUsername}> {username}</Text>
             </Text>
-            <Image source={{uri: '../../assets/Pika.png'}} style={{width: 250, height: 300, marginTop: 10,}} />
+            <Image source={{uri: '../../assets/william.png'}} style="width: 300, height: 300" />
             
         </View>
     );
