e6af120cc759730e33d04fec254774ca6e144226
Kun Zhu <zkzhu@uwaterloo.ca>
2022-09-18 03:39:21 -0400

    create friendList page

--- src/pages/FriendFinderScreen.js
+++ src/pages/FriendFinderScreen.js
@@ -1,54 +1,121 @@
 import React, { useState, useEffect } from "react";
 import { View, Image, Button, Text, StyleSheet } from "react-native";
 import { Camera, CameraType } from "expo-camera";
-//import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-/*
-<MapView
-  provider={PROVIDER_GOOGLE}
-  style={styles.map}
-  region={{
-    latitude: 43.4729,
-    longitude: -80.5396,
-    latitudeDelta: 0.015,
-    longitudeDelta: 0.0121,
-  }}
-/>;*/
+import React, { useState, useEffect, Fragment } from 'react';
+import { View , Image, Button, Text, StyleSheet} from "react-native";
+import { ListItem, Avatar } from "@react-native-material/core";
+import { Camera, CameraType } from 'expo-camera';
+import BritIcon from '../assets/BritIcon.png';
+import GalIcon from '../assets/GalIcon.png';
+import WiiIcon from '../assets/WiiIcon.png';
+import SteinIcon from '../assets/SteinIcon.png';
+import WillIcon from '../assets/WillIcon.png';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-const FriendFinderScreen = ({ navigation, route }) => {
-  return <View></View>;
-};
+
+const FriendFinderScreen = ( { navigation, route } ) => {
+ 
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    const forwardIcon = <Icon name={"forward"} size={30} style={{color: "grey"}}/>;
+    const dummy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
+    
+    return (
+        <View>
+            <Text style={styles.heading}>
+                Friends List
+            </Text>
+            <>
+                <ListItem
+                    leadingMode="avatar"
+                    title="brit"
+                    leading={<Avatar image={BritIcon}/>}
+                    secondaryText={dummy}
+                    trailing={forwardIcon}
+                    overline={"Level 2"}
+                    style={styles.listItem}
+                />
+                <ListItem
+                    leadingMode="avatar"
+                    title="am.i.will"
+                    leading={<Avatar image={WillIcon}/>}
+                    secondaryText={dummy}
+                    trailing={forwardIcon}
+                    overline={"Level 38438"}
+                />
+                <ListItem
+                    leadingMode="avatar"
+                    title="wi.i.am"
+                    leading={<Avatar image={WiiIcon}/>}
+                    secondaryText={dummy}
+                    trailing={forwardIcon}
+                    overline={"Level 873"}
+                />
+                <ListItem
+                    leadingMode="avatar"
+                    title="stein"
+                    leading={<Avatar image={SteinIcon}/>}
+                    secondaryText={dummy}
+                    trailing={forwardIcon}
+                    overline={"Level 1"}
+                />
+                <ListItem
+                    leadingMode="avatar"
+                    title="gal"
+                    leading={<Avatar image={GalIcon}/>}
+                    secondaryText={dummy}
+                    trailing={forwardIcon}
+                    overline={"Level 12"}
+                />
+                <Icon 
+                    name={"plus"} 
+                    size={50}
+                    style={{
+                        margin: "auto",
+                        display: "flex",
+                        padding: "25px",
+                        color: "grey",
+                    }}
+                />
+            </>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    overflow: "hidden",
-  },
-  camera: {
-    flex: 1,
-    width: "auto",
-    height: "auto",
-    position: "absolute",
-    top: "0px",
-    left: "0px",
-    //top: '-9999px',
-    //bottom: '-9999px',
-    //left: '-9999px',
-    //right: '-9999px',
-    margin: "auto",
-  },
-  main: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+    heading: {
+        fontSize: 25,
+        textAlign: "right",
+        padding: 15,
+        marginRight: 10,
+    },
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        overflow: 'hidden',
+    },
+    camera: {
+        flex: 1,
+        width: 'auto',
+        height: '100vh',
+        resizeMode: 'cover',
+        position: 'absolute',
+        top: '-9999px',
+        bottom: '-9999px',
+        left: '-9999px',
+        right: '-9999px',
+        margin: 'auto',
+    },
+    main: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    }
 });
 
 export default FriendFinderScreen;
